8ad5a74d47cbe85dacf0dc0a15bea43de5a4da87
Gary Long <dev@garylong.fr>
2017-01-22 17:36:58 +0100

    added tycho rcp build demo
    Added a demo project for an rcp build with tycho

---
+++ demo/demo-rcp-tycho/com.mds.bbl.tycho/tests/com.mds.bbl.tycho.rcp.tests/src/com/mds/bbl/tycho/rcp/tests/SampleTest.java
@@ -0,0 +1,14 @@
+package com.mds.bbl.tycho.rcp.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
+
