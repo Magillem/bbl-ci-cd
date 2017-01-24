dc7c42bf7e6798e2c7d9991368c475e33c66d587
Guillaume Dufour <guillaume.duff@gmail.com>
2017-01-24 15:11:31 +0100

    update launch demo

--- demo/launch.sh
+++ demo/launch.sh
@@ -1,10 +1,13 @@
-docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+docker network create jenkins2
+
+
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 --network="jenkins2" sonarqube
 #mvn sonar:sonar
 #With boot2docker:
 #mvn sonar:sonar -Dsonar.host.url=http://$(boot2docker ip):9000 -Dsonar.jdbc.url="jdbc:h2:tcp://$(boot2docker ip)/sonar"
 
 # use full access to localhost (quick and dirty, TODO use docker network)
-docker run -p 8080:8080 --network="host" jenkinsci/blueocean
+docker run -d --name blueocean -p 8080:8080 --network="jenkins2" jenkinsci/blueocean
 
 
 
